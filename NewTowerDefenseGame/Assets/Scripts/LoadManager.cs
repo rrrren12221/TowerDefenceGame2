c7b2ef36024a3fa8b5305150f684757a30540945
nishiumi <xiyeliang737@gmail.com>
2024-12-09 16:24:08 +0900

    ResetPlayerPrefs追加

--- NewTowerDefenseGame/Assets/Scripts/LoadManager.cs
+++ NewTowerDefenseGame/Assets/Scripts/LoadManager.cs
@@ -27,6 +27,20 @@ public class LoadManager : MonoBehaviour
         SceneManager.LoadScene(NishiScene);
     }
 
+    // リセットボタンなどで呼び出すメソッド
+    public void ResetPreferences()
+    {
+        // PlayerPrefsの全てのデータを削除
+        PlayerPrefs.DeleteAll();
+
+        // リセットされたことをログで表示
+        Debug.Log("PlayerPrefs has been reset.");
+
+        // リセット後に必要な処理を追加できます。
+        // 例えば、ゲームの再起動や初期化処理など
+    }
+
+
     //ゲームを終了
     public void EndGame()
     {
